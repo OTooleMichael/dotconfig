4e06ecb951ae589e0fd74ef7d7139b4d5becee3a
Michael O Toole <otoole.timmy@gmail.com>
2024-03-05 17:40:13 +0100

    Fsh is working better now

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,18 +10,32 @@ vim.keymap.set("n", "<leader>q", function()
   end
 end, { desc = "Close all unnamed buffers", remap = true })
 
+local function stripWhitespace(str)
+  return str:match("^%s*(.-)%s*$")
+end
+
+local function replaceRange(command, range_value)
+  return command:gsub("@(%a+)", function(letter)
+    if #letter ~= 1 then
+      return "@" .. letter
+    end
+    local value = vim.fn.getreg(letter)
+    if letter == "v" then
+      value = range_value
+    end
+    return vim.fn.shellescape(value)
+  end)
+end
+
 local function fshell_command(opts)
   local range_content = vim.fn.getline(vim.fn.getpos("'<")[2], vim.fn.getpos("'>")[2])
   local range_value = table.concat(range_content, "\n")
-  local command = table.concat(opts.fargs, " ")
-  -- if command contains "$r(ange)" replace it with the range content
-  if range_value ~= "" then
-    command = command:gsub("%$range", vim.fn.shellescape(range_value))
-  end
+  local command = stripWhitespace(table.concat(opts.fargs, " "))
+  command = replaceRange(command, range_value)
   if command == "" then
-    command = range_value
-    return
+    command = replaceRange(range_value, "")
   end
+  print("Running: " .. command)
 
   local buf_name = vim.api.nvim_buf_get_name(0)
   if buf_name ~= "" then
@@ -29,22 +43,25 @@ local function fshell_command(opts)
   end
   -- get the current active buffer
   local buffer = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_lines(buffer, -1, -1, false, { "Running: ", command })
   local function handle_output(job_id, data, event)
-    if event == "stdout" then
-      -- Append the output to a buffer
+    if data then
       vim.api.nvim_buf_set_lines(buffer, -1, -1, false, data)
     end
   end
   local job_id = vim.fn.jobstart(command, {
     on_stdout = handle_output,
-    on_strderr = handle_output,
-    stderr_buffered = false,
+    on_stderr = handle_output,
+    stderr_buffered = true,
     stderr_buffer = buffer,
-    stdout_buffered = false,
+    stdout_buffered = true,
     stdout_buffer = buffer,
   })
-
-  -- vim.cmd(": % ! " .. command)
+  vim.api.nvim_buf_attach(buffer, false, {
+    on_detach = function()
+      vim.fn.jobstop(job_id)
+    end,
+  })
 end
 
 vim.api.nvim_create_user_command("Fshell", fshell_command, { range = true, nargs = "*" })
@@ -88,6 +105,7 @@ vim.keymap.set("n", "<leader>o", "o<Esc>")
 vim.keymap.set("n", "<leader>O", "O<Esc>")
 vim.keymap.set("n", "<leader>rf", ":Fscript format<CR>")
 vim.keymap.set("n", "<leader>rt", ":Fscript test<CR>")
+vim.keymap.set("v", "<leader>re", ":Fsh<CR>")
 vim.keymap.set("n", "<leader>gp", ":ChatGPT<CR>")
 vim.keymap.set("v", "<leader>gp", ":ChatGPTEditWithInstructions<CR>")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { silent = true })
