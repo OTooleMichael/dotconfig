4e1a7097f8bc2fe949f2d1ca62c64c6c731ac183
Michael O Toole <otoole.timmy@gmail.com>
2024-09-04 09:37:11 +0200

    init.sh cleanup

--- scripts/init_mac.sh
+++ scripts/init_mac.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 set -eo pipefail
 brew update
-brew install fzf ripgrep bat zoxide
+brew install fzf ripgrep bat zoxide neovim zoxide zellij
 brew cleanup
 brew install --cask font-jetbrains-mono-nerd-font
 #install kitty
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-ln -s ~/.zshrc ~/.config/.zshrc
-brew install zoxide
-```
-### Step 3: Configure Your Shell
-After installing Zoxide, you need to configure your shell to use it. The configuration steps depend on the shell you are using (e.g., `bash`, `zsh`, `fish`).
-#### For `bash`:
-Add the following lines to your `~/.bashrc` or `~/.bash_profile`:
-```sh
-eval "$(zoxide init bash)"
-```
-#### For `zsh`:
-Add the following lines to your `~/.zshrc`:
-```sh
+# curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -s -f ~/.config/.zshrc ~/.zshrc
+set e
 eval "$(zoxide init zsh)"
+set -e
+echo "Complete"
