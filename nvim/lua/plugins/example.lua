c880569941af3bf49851d2aa5de5f8008bb3a5b7
Michael O Toole <otoole.timmy@gmail.com>
2024-12-28 13:09:32 +0100

    Zellij, and Lazy updates

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -13,65 +13,27 @@ return {
     end,
   },
   {
-    "neovim/nvim-lspconfig",
-    version = "6e5c78ebc9936ca74add66bda22c566f951b6ee5",
+    "saghen/blink.cmp",
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "*",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
     opts = {
-      inlay_hints = {
-        enabled = false,
+      keymap = {
+        preset = "default",
+        ["<C-a>"] = { "select_and_accept" },
       },
-      servers = {
-        basedpyright = {
-          typeCheckingMode = "standard",
-        },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
       },
     },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-      local cmp = require("cmp")
-      local defaults = require("cmp.config.default")()
-      return {
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        completion = {
-          completeopt = "menu,menuone,noinsert",
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<C-a>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "path" },
-        }, {
-          { name = "buffer" },
-        }),
-        formatting = {
-          format = function(_, item)
-            local icons = require("lazyvim.config").icons.kinds
-            if icons[item.kind] then
-              item.kind = icons[item.kind] .. item.kind
-            end
-            return item
-          end,
-        },
-        experimental = {
-          ghost_text = {
-            hl_group = "CmpGhostText",
-          },
-        },
-        sorting = defaults.sorting,
-      }
-    end,
+    opts_extend = { "sources.default" },
   },
   {
     "echasnovski/mini.surround",
